fb9a05ecd8acb0ac84c660f1152ae151cc0c9486
Kunal <mundadakunal25@gmail.com>
2024-10-10 23:23:46 -0700

    Change to `vue create .` for setup - Revert vite install from #1 - Make changes to frontend to test setup

--- frontend/src/components/FooterPart.vue
+++ frontend/src/components/FooterPart.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="home">
-    <h1>Welcome to My App</h1>
-    <p>This is the home page.</p>
-  </div>
+  <footer>
+    <p>&copy; 2024 My App</p>
+  </footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "HomeView",
+  name: "FooterPart",
 });
 </script>
--- frontend/src/components/MainContent.vue
+++ frontend/src/components/MainContent.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="home">
-    <h1>Welcome to My App</h1>
-    <p>This is the home page.</p>
-  </div>
+  <main>
+    <router-view></router-view>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "HomeView",
+  name: "MainContent",
 });
 </script>
